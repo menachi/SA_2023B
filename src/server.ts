d6f883ba0ecf781a6ebee952bcf2f80c799b124b
Eliav Menachi <eliav.menachi@gmail.com>
2024-01-18 19:28:08 +0200

    https

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import initApp from "./app";
-// import https from 'https';
+import https from 'https';
 import http from 'http';
-// import fs from 'fs';
+import fs from 'fs';
 import swaggerUI from "swagger-ui-express"
 import swaggerJsDoc from "swagger-jsdoc"
 
@@ -24,10 +24,12 @@ initApp().then((app) => {
   if (process.env.NODE_ENV !== 'production') {
     console.log('development');
     http.createServer(app).listen(process.env.PORT);
+  } else {
+    console.log('PRODUCTION');
+    const options2 = {
+      key: fs.readFileSync('../client-key.pem'),
+      cert: fs.readFileSync('../client-cert.pem')
+    };
+    https.createServer(options2, app).listen(process.env.HTTPS_PORT);
   }
-  // const options2 = {
-  //   key: fs.readFileSync('../client-key.pem'),
-  //   cert: fs.readFileSync('../client-cert.pem')
-  // };
-  // https.createServer(options2, app).listen(process.env.HTTPS_PORT);
 });
